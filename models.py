8adf34d1f56285fb89c1fe3c69c6d4efe6b8d694
Naichuan Zhang <nzhang@bu.edu>
2017-09-23 10:40:25 -0400

    push backend logic of the bitcoin game

--- models.py
+++ models.py
@@ -11,9 +11,16 @@ class ProfileInfo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('User.id'))
     picture = db.Column(db.BLOB)
+    game_bit_balance = db.Column(db.Float)
+    game_usd_balance = db.Column(db.Float)
 
 class GameInfo(db.Model):
     round_id = db.Column(db.Integer, primary_key = True)
     round_time = db.Column(db.DateTime, unique = True)
     user_id = db.Column(db.Integer, db.ForeignKey('User.id'))
-    
+
+class UserGameTransactions(db.Model):
+    transaction_id = db.Column(db.Integer, primary_key = True)
+    game_round_id = db.Column(db.Integer, db.ForeignKey('GameInfo.round_id'))
+    amout = db.Column(db.Float)
+    transaction_type = db.Column(db.Boolean)
