54ea45f641595e5e4c5f2a926fc16c827422a535
Naichuan Zhang <nzhang@bu.edu>
2017-09-23 10:32:44 -0400

    add new models

--- models.py
+++ models.py
@@ -6,3 +6,14 @@ class User(UserMixin,db.Model):
     username = db.Column(db.String(15), unique=True)
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(80))
+
+class ProfileInfo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('User.id'))
+    picture = db.Column(db.BLOB)
+
+class GameInfo(db.Model):
+    round_id = db.Column(db.Integer, primary_key = True)
+    round_time = db.Column(db.DateTime, unique = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('User.id'))
+    
